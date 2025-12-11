4afb311ec65965c5539421b216297242ff401141
pete414n <pete414n@gmail.com>
2025-12-11 09:38:50 +0100

    hello world with args and tests

---
+++ 01_hello/hello.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Author: pete414n
+Purpose: Say Hello
+"""
+
+import argparse
+
+
+def get_args():
+    """Get the command-line arguments"""
+
+    parser = argparse.ArgumentParser(description='Say Hello')
+    parser.add_argument('-n', '--name', metavar='name',
+                        default='World', help='Name to greet')
+    return parser.parse_args()
+
+
+def main():
+    """Main"""
+
+    args = get_args()
+    print('Hello, ' + args.name + '!')
+
+
+if __name__ == '__main__':
+    main()
